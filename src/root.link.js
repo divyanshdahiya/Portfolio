443b0665e5576ddb9dfab76bf5e845a802deacb0
divyanshdahiya <dahiya.divyansh1@gmail.com>
2022-02-18 11:51:23 +0530

    final

---
+++ src/root.link.js
@@ -0,0 +1,12 @@
+export const RESUME_PATH =
+  "https://drive.google.com/file/d/1qB6k0lRtNwtgAeZhMgS2JsEfQbD8Smd9/view?usp=sharing";
+export const GITHUB_USERNAME = "divyanshdahiya";
+export const LINKEDIN_USERNAME = "divyanshdahiya";
+export const FIRST_NAME = "Divyansh";
+export const LAST_NAME = "Dahiya";
+export const ADDRESS = "New Delhi, India";
+export const EMAIL_ID = "dahiya.divyansh1@gmail.com";
+export const TWITTER_ID = "";
+export const FRONT_PAGE_NAME = "Divyansh";
+export const FRONT_PAGE_DESC =
+  "A Full-Stack Web Developer & Open Source Contributor.I like to build things for web and solving problems.";
